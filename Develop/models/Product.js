3be04ee37aab7f4dfe623e90086fa56a9d1e7265
efogerson1 <efogerson2@gmail.com>
2023-07-29 12:33:01 -0700

    finished models references

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -35,7 +35,7 @@ Product.init(
         isNumeric: true
       }
     },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,14 +7,34 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 Product.belongsTo(Category, {
-  foriegnKey: 'category_id'
+  foriegnKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as: 'tags',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  as: 'products',
+  foreignKey: 'product_id'
+});
+
+
 
 module.exports = {
   Product,
