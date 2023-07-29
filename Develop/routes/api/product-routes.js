e384d7d4ac930b074a8496fbb7834e10ab9f0294
efogerson1 <efogerson2@gmail.com>
2023-07-29 13:59:19 -0700

    finalized product-route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+try {
+  const productInfo = await Product.findAll({
+      include: [{
+        model: Category
+      },
+      {
+        model: Tag,
+        as: 'tags'
+      }
+    ]
+  });
+  res.json(productInfo);
+} catch (error){
+  res.status(500).json(err)
+}
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, as: "tags"}]
+    })
+    res.json(product);
+  } catch (error){
+    res.status(500).json({message: "Server error"})
+  }
 });
 
 // create new product
@@ -92,8 +115,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-});
+  try {
+    const productUpdate = await Product.destroy({
+    where: {id: req.params.id}
+    }
+    );
+    res.status(201).json(productUpdate);
+      } catch (error) {
+        res.status(500).json(`{"message: "Error deleting product"}`)
+    }
+    });
 
 module.exports = router;
