a649ef7030405cf92452cdafaa3816df56d7cc08
efogerson1 <efogerson2@gmail.com>
2023-07-31 16:38:14 -0700

    typo for require, fixed

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = reqire('./config/connection')
+const sequelize = require('./config/connection.js')
 // import sequelize connection
 
 const app = express();
